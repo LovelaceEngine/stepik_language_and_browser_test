9ccf701e1daa735341c337ee91c4403345b1575f
LovelaceEngine <227027502+LovelaceEngine@users.noreply.github.com>
2025-09-28 23:08:53 +0300

    Добавил тест-проверку наличия кнопки 'Добавить в корзину'

---
+++ test_items.py
@@ -0,0 +1,22 @@
+import pytest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_presence_of_a_button(browser):
+    """Тест для проверки наличия кнопки добавления в корзину"""
+
+    browser.get(link)
+    #time.sleep(30)
+
+    button = WebDriverWait(browser, 10).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, ".btn-add-to-basket"))
+    )
+
+    print (f"Найдена кнопка '{button.text}'") # для удобства, -s в начале
+    assert button, f"Кнопка добавления в карзину отсутсвует"
+
